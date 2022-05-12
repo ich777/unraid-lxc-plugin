540fd50c1997e247aac3342c585e30d4261d5a09
ich777 <christophhummer@gmail.com>
2022-05-12 18:22:06 +0200

    „source/makepkg-unraid.sh“ hinzufügen

---
+++ source/makepkg-unraid.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PLUGIN_NAME="lxc"
+BASE_DIR="/usr/local/emhttp/plugins"
+TMP_DIR="/tmp/${PLUGIN_NAME}_"$(echo $RANDOM)""
+VERSION="$(date +'%Y.%m.%d')"
+mkdir -p $TMP_DIR/$VERSION
+cd $TMP_DIR/$VERSION
+cp --parents -R $BASE_DIR/$PLUGIN_NAME/ $TMP_DIR/$VERSION/
+chmod -R 755 $TMP_DIR/$VERSION/
+rm $TMP_DIR/$VERSION/$BASE_DIR/$PLUGIN_NAME/README.md
+makepkg -l y -c y $TMP_DIR/gvt.g-$VERSION.txz
+md5sum $TMP_DIR/gvt.g-$VERSION.txz | awk '{print $1}' > $TMP_DIR/gvt.g-$VERSION.txz.md5
+rm -R $TMP_DIR/$VERSION/
+chmod 755 $TMP_DIR/*
