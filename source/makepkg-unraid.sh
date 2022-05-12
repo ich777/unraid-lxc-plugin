4a9114d5de59db5c422c74f25e77f2ee70c23c4e
ich777 <christophhummer@gmail.com>
2022-05-12 20:29:28 +0200

    „source/makepkg-unraid.sh“ ändern

--- source/makepkg-unraid.sh
+++ source/makepkg-unraid.sh
@@ -8,7 +8,7 @@ cd $TMP_DIR/$VERSION
 cp --parents -R $BASE_DIR/$PLUGIN_NAME/ $TMP_DIR/$VERSION/
 chmod -R 755 $TMP_DIR/$VERSION/
 rm $TMP_DIR/$VERSION/$BASE_DIR/$PLUGIN_NAME/README.md
-makepkg -l y -c y $TMP_DIR/gvt.g-$VERSION.txz
-md5sum $TMP_DIR/gvt.g-$VERSION.txz | awk '{print $1}' > $TMP_DIR/gvt.g-$VERSION.txz.md5
+makepkg -l y -c y $TMP_DIR/${PLUGIN_NAME}-$VERSION.txz
+md5sum $TMP_DIR/${PLUGIN_NAME}-$VERSION.txz | awk '{print $1}' > $TMP_DIR/${PLUGIN_NAME}-$VERSION.txz.md5
 rm -R $TMP_DIR/$VERSION/
 chmod 755 $TMP_DIR/*
