8999546dfdc8114caa268de56e08fd9e40f5b3da
ich777 <christophhummer@gmail.com>
2023-10-04 14:45:27 +0200

    Dateien nach "source/usr/local/emhttp/plugins/lxc/lxc_scripts" hochladen

--- source/usr/local/emhttp/plugins/lxc/lxc_scripts/lxc-startup.sh
+++ source/usr/local/emhttp/plugins/lxc/lxc_scripts/lxc-startup.sh
@@ -4,7 +4,5 @@ AUTOSTART_DELAY="$(cat /boot/config/plugins/lxc/plugin.cfg | grep -n "^AUTOSTART
 logger "LXC: Waiting ${AUTOSTART_DELAY}s for autostart from container(s) in background"
 sleep ${AUTOSTART_DELAY:=10}s 2>/dev/null
 
-if [ "$(cat /boot/config/plugins/lxc/plugin.cfg | grep -n "^SERVICE=" | cut -d '=' -f2 | sed 's/\"//g')" == "enabled" ]; then
-  logger "LXC: Executing autostart from container(s)"
-  lxc-autostart
-fi
+logger "LXC: Executing autostart from container(s)"
+lxc-autostart
